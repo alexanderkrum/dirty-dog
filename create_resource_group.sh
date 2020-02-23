16366cd4b7e49bdad58cf9612b33bc63bf93ffa8
Alexander Krum <alexander.krum.it@gmail.com>
2020-02-23 13:50:26 +1100

    Update create resource group script

--- create_resource_group.sh
+++ create_resource_group.sh
@@ -3,5 +3,5 @@
 read -p "Resource name: " resource_name
 az account list-locations | jq -r '.[] | .name'
 read -r -p "Location name: " location_name
-echo 'Creating resource $resource_name in $location_name .....'
+echo "Creating resource group $resource_name in $location_name ....."
 az group create -n $resource_name -l $location_name
